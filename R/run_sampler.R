f4aae3e9580d6483590f39e5e9187fe9bbe2416d
mkomod <mkomodromos01@gmail.com>
2021-06-07 13:42:33 +0100

    add: log pl func, up: run sampler

--- R/run_sampler.R
+++ R/run_sampler.R
@@ -22,18 +22,40 @@
 #' run_sampler(Y, delta, X, 2)
 #'
 #' @export
-run_sampler <- function(Y, delta, X, lambda, a_0=1, b_0=ncol(X), kernel_sd=0.2, 
-	mcmc_samples=5e3, verbose=T)
+run_sampler <- function(Y, delta, X, lambda, a_0=1, b_0=ncol(X), kernel_sd=0.2,
+	kernel_scale=10, mcmc_samples=5e3, verbose=T)
 {
     if (!is.matrix(X)) stop("X is not a matrix") 
     if (lambda < 0) stop("lambda must be greater than 0")
     if (kernel_sd < 0) stop("kernel_sd must be greater than 0")
+    if (mcmc_samples <= 50) stop("mcmc_samples must be greater than 50")
 
     Y_sorted <- order(Y) - 1           # C++ index is 0-based, R is 1-based
     Y_failure <- which(delta[Y_sorted + 1] == 1) - 1
 
-    return(
-	sampler(Y_sorted, Y_failure, X, lambda, kernel_sd, mcmc_samples, verbose)
-    )
+    res <- sampler(Y_sorted, Y_failure, X, lambda, a_0, b_0, 
+	    kernel_sd, kernel_scale, mcmc_samples, verbose)
+
+    return(res)
+}
+
+
+#' Compute the log partial-likelihood 
+#'
+#' @param Y observed failure / censoring times
+#' @param delta censoring vector (0: censored, 1: uncensored)
+#' @param X matrix of observations
+#' @param b vector of coefficients
+#'
+#' @export
+log_partial_likelihood <- function(Y, delta, X, b)
+{
+    if (!is.matrix(X)) stop("X is not a matrix") 
+    
+    # C++ index is 0-based, R is 1-based
+    Y_sorted <- order(Y) - 1 
+    Y_failure <- which(delta[Y_sorted + 1] == 1) - 1
+
+    return(log_PL(X, b, Y_sorted, Y_failure))
 }
 
