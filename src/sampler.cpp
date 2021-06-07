75bc09184daeb20604b816082438d80198dc2087
mkomod <mkomodromos01@gmail.com>
2021-06-07 13:29:08 +0100

    ft: use Gibb MH over HMC

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -5,7 +5,7 @@
 // [[Rcpp::export]]
 Rcpp::List sampler(arma::uvec Y_sorted, arma::uvec Y_failure, arma::mat X, 
 	double lambda, double a_0, double b_0, double kernel_sd, 
-	arma::uword mcmc_samples, bool verbose, double e, int L)
+	double kernel_scale, arma::uword mcmc_samples, bool verbose)
 {
     arma::uword p = X.n_cols;
     arma::uword n = X.n_rows;
@@ -21,15 +21,22 @@ Rcpp::List sampler(arma::uvec Y_sorted, arma::uvec Y_failure, arma::mat X,
     arma::vec z = arma::vec(p, arma::fill::randu);
     z = arma::round(z);
 
+    arma::uvec js = arma::uvec(p, arma::fill::zeros);
+    for (arma::uword i = 0; i < p; ++i)
+	js(i) = i;
+
+
     if (verbose)
 	Rcpp::Rcout << "\n Running sampler    [";
 
     for (arma::uword iter = 0; iter < mcmc_samples; ++iter) {
+
 	// update w
 	w = R::rbeta(a_0, b_0);
 
 	// update p
-	for (arma::uword j = 0; j < p; ++j) {
+	js = shuffle(js);
+	for (arma::uword j : js) {
 	    z(j) = 0;
 	    double p0 = log(1-w) + log_PL(X, b % z, Y_sorted, Y_failure); 
 	    z(j) = 1;
@@ -43,35 +50,24 @@ Rcpp::List sampler(arma::uvec Y_sorted, arma::uvec Y_failure, arma::mat X,
 	}
 
 	// update b
-	// for (arma::uword j = 0; j < p; ++j) {
-	//     double b_old = b(j);
-	//     double b_new = R::rnorm(b_old, kernel_sd * pow(10.0 / lambda, (1.0 - z(j))));
-	//     
-	//     // MH denominator
-	//     double den = log_PL(X, b % z, Y_sorted, Y_failure) +
-	// 		log_Laplace(b_old, lambda);
-	//     
-	//     // MH numerator
-	//     b(j) = b_new;
-	//     double num = log_PL(X, b % z, Y_sorted, Y_failure) +
-	// 		log_Laplace(b_new, lambda);
+	js = shuffle(js);
+	for (arma::uword j : js) {
+	    double b_old = b(j);
+	    double b_new = R::rnorm(b_old, kernel_sd * pow(L, (1.0 - z(j))));
+	    
+	    // MH denominator
+	    double den = log_PL(X, b % z, Y_sorted, Y_failure) +
+			log_Laplace(b_old, lambda);
+	    
+	    // MH numerator
+	    b(j) = b_new;
+	    double num = log_PL(X, b % z, Y_sorted, Y_failure) +
+			log_Laplace(b_new, lambda);
 
-	//     double a = std::min(1.0, exp(num - den));
-	//     if (a <= R::runif(0, 1)) b(j) = b_old; 
-	// }
-
-	// update b
-	arma::vec r = arma::vec(p, arma::fill::randn);
-	arma::vec r_old = r;
-	arma::vec b_new = b % z;
-	for (int l = 0; l < L; ++l) {
-	    leapfrog(b_new, r, e, X, Y_sorted, Y_failure, lambda);
+	    double a = std::min(1.0, exp(num - den));
+	    if (a <= R::runif(0, 1)) b(j) = b_old; 
 	}
-	double num = log_PL(X, b_new, Y_sorted, Y_failure) - 0.5 * dot(r, r);
-	double den = log_PL(X, b, Y_sorted, Y_failure) - 0.5 * dot(r_old, r_old);
-	double a = exp(num - den);
-	if (a > R::runif(0, 1)) b = b_new;
-	
+
 	W(iter) = w;
 	B.col(iter) = b;
 	Z.col(iter) = z;
@@ -86,9 +82,9 @@ Rcpp::List sampler(arma::uvec Y_sorted, arma::uvec Y_failure, arma::mat X,
 	Rcpp::Rcout << "]\n\n";
 
     return Rcpp::List::create(
-	Rcpp::Named("w") = W,
 	Rcpp::Named("b") = B,
-	Rcpp::Named("z") = Z
+	Rcpp::Named("z") = Z,
+	Rcpp::Named("w") = W
     );
 }
 
